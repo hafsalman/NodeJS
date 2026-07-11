d197002162e72bbd36be5dddc7d6bbdbf7d4f4b2
Hafsa Salman <hafsalman0521@gmail.com>
2026-07-11 12:31:02 +0500

    Req.end for favicon

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const myServer = http.createServer((req, res) => {
     // console.log("New Request Received");
     // console.log(req);
 
+    if (req.url === '/favicon.ico') return res.end();
+
     const log = `${Date.now()}: ${req.url} New Request Received\n`;
     fs.appendFile('log.txt', log, (err, data) =>{
         // res.end("Hello from server");
